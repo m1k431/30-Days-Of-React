887bb6eda00fe75b417310a3ac24714e10f80fa0
m1k431 <garcia.mikaelgmail.com>
2020-10-05 13:49:46 +0200

    level1

---
+++ solutions/day-01/level1/level1.js
@@ -0,0 +1,24 @@
+var tableauVide = []
+
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya',
+]
+
+console.log(countries)
+
+console.log(countries.length)
+
+console.log(countries[0])
+
+console.log(countries[10])
+
