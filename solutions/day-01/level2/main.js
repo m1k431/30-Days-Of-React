abaef2eeb92b27ae84b627155a03df7384f83b61
m1k431 <garcia.mikaelgmail.com>
2020-10-05 15:01:34 +0200

    level2

--- solutions/day-01/level2/main.js
+++ solutions/day-01/level2/main.js
@@ -1,6 +1,40 @@
-import { countries } from './countries.js';
-import { web_techs } from './web_techs.js';
+/*jshint esversion: 6 */
+console.log('pays: ' + countries);
+console.log('techs: ' + webTechs);
 
-let pays = { countries };
+//no punctuation to array
+let text = 'I love teaching and empowering people. I teach HTML, CSS, JS, REACT, Python';
 
+const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+const textPasPonctue = text.replace(regex, '');
 
+let tabText = textPasPonctue.split(' ');
+console.log(tabText);
+
+//add remove edit item
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+shoppingCart[2] = 'Green Tea';
+
+//add before
+shoppingCart.splice(0, 0, 'Meat');
+
+//add at end
+shoppingCart.push('Sugar');
+
+console.log(shoppingCart);
+
+function ethiopia(pays) {
+  return pays == 'Ethiopia';
+}
+
+console.log(countries.find(ethiopia));
+
+function sass(techs) {
+  return (techs == 'Sass');
+}
+
+if (webTechs.find(sass) == undefined) {
+  console.log('Sass is a CSS preprocessor');
+  webTechs.push('Sass');
+  console.log(webTechs);
+}
